f487ab03dcb4739ebe6eccebfe563f0ab6e4f488
Kevin Baugh <loraxx753@gmail.com>
2016-07-12 22:40:48 -0500

    changed configs to load from file

--- web/app.php
+++ web/app.php
@@ -8,12 +8,19 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$env = (isset($_SERVER['SYMFONY_ENV'])) ? $_SERVER['SYMFONY_ENV'] : 'dev';
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+$debug = (in_array($env, ['dev', 'test', 'local']) && $_SERVER['SYMFONY_DEBUG']) ? true : false;
+
+$kernel = new AppKernel($env, $debug);
+if ( !in_array($env, ['dev', 'local']) ) {
+    $kernel->loadClassCache();
+    $kernel = new AppCache($kernel);
+
+    // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+    Request::enableHttpMethodParameterOverride();
+
+}
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
